787e00d11eb653d369a700b9db4a429b50a1a1fc
kesylo <32457792+kesylo@users.noreply.github.com>
2020-04-19 23:04:10 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,9 +14,9 @@ exec 3>&1 1>>output.log 2>&1
 # install docker
 printf 'Installing docker...\n\n' 1>&3
 
-sudo apt remove --yes docker docker-engine docker.io containerd runc \
-    && sudo apt update \
-    && sudo apt --yes --no-install-recommends install \
+sudo apt-get remove --yes docker docker-engine docker.io containerd runc \
+    && sudo apt-get update \
+    && sudo apt-get --yes --no-install-recommends install \
         apt-transport-https \
         ca-certificates \
     && wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg \
@@ -25,8 +25,8 @@ sudo apt remove --yes docker docker-engine docker.io containerd runc \
         "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu \
         $(lsb_release --codename --short) \
         stable" \
-    && sudo apt update \
-    && sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io \
+    && sudo apt-get update \
+    && sudo apt-get --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io \
     && sudo usermod --append --groups docker "$USER" \
     && sudo systemctl enable docker \
     && printf '\nDocker installed successfully\n\n'
@@ -60,4 +60,3 @@ curl localhost:9200/_cat/health 1>&3
 
 # cleanup
 sudo rm docker-compose.yml
-
