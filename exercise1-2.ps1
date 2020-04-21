a54ba5dc919cb8a1ea6e608dde88560e9c3e989a
kesylo <32457792+kesylo@users.noreply.github.com>
2020-04-21 23:13:05 +0200

    Add files via upload

---
+++ exercise1-2.ps1
@@ -0,0 +1,42 @@
+﻿$vms = Get-AzVM -ResourceGroupName Exercise1-RG
+
+$et=Get-Date
+
+$st=$et.AddDays(-3)
+
+$arr =@()
+
+ foreach($vm in $vms)
+ {
+ #define a string to store related infomation like vm name etc. then add the string to an array
+ $s = ""
+
+ $cpu = Get-AzMetric -ResourceId $vm.Id -MetricName "Percentage CPU" -DetailedOutput `
+ -TimeGrain 12:00:00  -WarningAction SilentlyContinue
+
+ $diskReadBytes = Get-AzMetric -ResourceId $vm.Id -MetricName "Disk Read Bytes" -DetailedOutput `
+ -TimeGrain 00:01:00  -WarningAction SilentlyContinue
+
+ $cpu_total=0.0
+ $diskReadBytes=0.0
+
+  foreach($c in $cpu.Data.Average)
+ {
+  #this is a average value for 12 hours, so total = $c*12 (or should be $c*12*60*60)
+  $cpu_total += $c*12
+ }
+
+ foreach($d in $diskReadBytes.Data.Total)
+ {
+ $diskReadBytes += $d 
+ }
+
+ # add all the related info to the string
+ $s = "VM Name: " + $vm.name + "; CPU: " +$cpu_total +"; Disk Read Bytes : " + $diskReadBytes + ""
+
+  # add the above string to an array
+ $arr += $s
+ }
+
+ #check the values in the array
+ $arr
---
+++ exercise1.ps1
@@ -0,0 +1,129 @@
+﻿#Import-Module Az
+
+#Login-AzAccount
+
+#Define the following parameters for the virtual machine.
+$vmAdminUsername = "KesyloKingloic"
+$vmAdminPassword = ConvertTo-SecureString "KesyloKing@password" -AsPlainText -Force
+ 
+#Define the following parameters for the Azure resources.
+$azureLocation              = "EastUS2"
+$azureResourceGroup         = "Exercise1-RG"
+$azureVmName                = @("VM-01","VM-02")
+$azureVmOsDiskName          = "DISK-"
+$azureVmSize                = "Standard_D2s_v3"
+$azureVmnetwork             = "192.168.1.0/24"
+ 
+#Define the networking information.
+$azureNicName               = "NIC-"
+ 
+#Define the existing VNet information.
+$azureVnetName              = "Exercise1-Vnet"
+$azureVnetSubnetName        = "Exercise1-Subnet"
+ 
+#Define the VM marketplace image details.
+$azureVmPublisherName = "MicrosoftWindowsServer"
+$azureVmOffer = "WindowsServer"
+$azureVmSkus = "2019-Datacenter"
+
+# check if ressource group exists
+Get-AzResourceGroup -Name $azureResourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent)
+{
+    # ResourceGroup doesn't exist
+    New-AzResourceGroup -ResourceGroupName $azureResourceGroup -Location $azureLocation
+}
+
+
+$subnetConfig = New-AzVirtualNetworkSubnetConfig `
+  -Name $azureVnetSubnetName `
+  -AddressPrefix $azureVmnetwork
+
+$vnet = New-AzVirtualNetwork `
+  -ResourceGroupName $azureResourceGroup `
+  -Location $azureLocation `
+  -Name $azureVnetName `
+  -AddressPrefix $azureVmnetwork `
+  -Subnet $subnetConfig
+
+#Get the subnet details for the specified virtual network + subnet combination.
+$azureVnetSubnet = (Get-AzVirtualNetwork -Name $azureVnetName -ResourceGroupName $azureResourceGroup).Subnets | Where-Object {$_.Name -eq $azureVnetSubnetName}
+ 
+#Create the public IP address.
+$azurePublicIp1 = New-AzPublicIpAddress -Name "PubIP1" -ResourceGroupName $azureResourceGroup -Location $azureLocation -AllocationMethod Static
+
+$azurePublicIp2 = New-AzPublicIpAddress -Name "PubIP2" -ResourceGroupName $azureResourceGroup -Location $azureLocation -AllocationMethod Static
+
+$publicIPs = @()
+$publicIPs += $azurePublicIp1
+$publicIPs += $azurePublicIp2
+
+#Store the credentials for the local admin account.
+$vmCredential = New-Object System.Management.Automation.PSCredential ($vmAdminUsername, $vmAdminPassword)
+
+
+$nsgRule = New-AzNetworkSecurityRuleConfig `
+  -Name AllowICMP `
+  -Protocol Icmp `
+  -Direction Inbound `
+  -Priority 1000 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange * `
+  -Access Allow
+
+# Create a network security group using the rule
+$nsg = New-AzNetworkSecurityGroup `
+  -ResourceGroupName $azureResourceGroup `
+  -Location $azureLocation `
+  -Name NetworkSecurityGroup `
+  -SecurityRules $nsgRule
+
+
+
+for($i = 0; $i -le $azureVmName.count -1; $i++)  
+{
+ 
+ # create NICs
+ $NIC = New-AzNetworkInterface -Name ($azureNicName+$azureVmName[$i]) `
+                               -ResourceGroupName $azureResourceGroup `
+                               -Location $azureLocation `
+                               -SubnetId $azureVnetSubnet.Id `
+                               -PublicIpAddressId $publicIPs[$i].Id `
+                               -NetworkSecurityGroupId $nsg.Id
+
+ 
+ $VirtualMachine = New-AzVMConfig -VMName $azureVmName[$i] `
+                                  -VMSize $azureVmSize
+
+ $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine `
+                                           -Windows `
+                                           -ComputerName $azureVmName[$i] `
+                                           -Credential $vmCredential `
+                                           -ProvisionVMAgent  `
+                                           -EnableAutoUpdate
+ 
+ $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine `
+                                            -Id $NIC.Id
+
+ $VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine `
+                                       -PublisherName $azureVmPublisherName `
+                                       -Offer $azureVmOffer `
+                                       -Skus $azureVmSkus `
+                                       -Version "latest"
+                                       
+ $VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine `
+                                  -StorageAccountType "Premium_LRS" `
+                                  -Caching ReadWrite `
+                                  -Name ($azureVmOsDiskName+$azureVmName[$i])`
+                                  -CreateOption FromImage
+ 
+ New-AzVM -ResourceGroupName $azureResourceGroup `
+          -Location $azureLocation `
+          -VM $VirtualMachine `
+          -Verbose
+}
+
+
